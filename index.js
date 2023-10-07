8567cf5a596f401cbaa9136afea23c83ae385054
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-07 20:09:59 +0300

    [200~Add test cases for writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+ 
+  
+  
+  
+  
+  
